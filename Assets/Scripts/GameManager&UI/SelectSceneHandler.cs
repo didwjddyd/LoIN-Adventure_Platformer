32f83f3271512b480ceb7471868719abb73509ea
mins-oo <msjhkim04@naver.com>
2023-12-27 03:57:25 +0900

    feat: 스테이지 선택 씬 추가
    스테이지 선택 씬 UI 제작
    
    SelectSceneHandler.cs 추가
    책갈피(Button)를 클릭하여 스테이지 선택 및 해당 스테이지 정보 출력
    게임시작 버튼을 눌러 해당 Scene으로 전환
    clearState 변수를 통해 스테이지 클리어 유무에 따라 도장이나 자물쇠를 추가

---
+++ Assets/Scripts/GameManager&UI/SelectSceneHandler.cs
@@ -0,0 +1,216 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SelectSceneHandler : MonoBehaviour
+{
+    [Header("Image Sprite")]
+    public Sprite disableMark;
+    public Sprite enableMark;
+    public Sprite offStar;
+    public Sprite onStar;
+
+    [Header("Bookmark")]
+    public GameObject stage1Button;
+    public GameObject stage2Button;
+    public GameObject stage3Button;
+
+    [Header("Timer")]
+    public Text time;
+
+    [Header("Enemy")]
+    public GameObject stage1Enemy;
+    public GameObject stage2Enemy;
+    public GameObject stage3Enemy;
+
+    [Header("Stage Clear Recognition")]
+    public GameObject stamp;
+    public GameObject locker;
+
+    [Header("Stage Infomation")]
+    public Text subText;
+    public Text numText;
+    public Text infoText;
+    public List<GameObject> stars;
+
+    [Header("Transition")]
+    public Slider transition;
+
+    string selectedStage = "Stage1";
+    int clearState = 0;
+    bool isTransition = false;
+
+    private void Start()
+    {
+        OnStage1Button();
+    }
+
+    void SetStar(int n)
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            if (i < n)
+                stars[i].GetComponent<Image>().sprite = onStar;
+            else
+                stars[i].GetComponent<Image>().sprite = offStar;
+        }
+    }
+
+    public void OnStage1Button()
+    {
+        if (!isTransition && selectedStage != "Stage1")
+        {
+            selectedStage = "Stage1";
+            StartCoroutine("Fadeout");
+        }
+        else
+        {
+            stage1Button.GetComponent<Image>().sprite = enableMark;
+            stage2Button.GetComponent<Image>().sprite = disableMark;
+            stage3Button.GetComponent<Image>().sprite = disableMark;
+
+            time.text = "10:00";
+
+            stage1Enemy.SetActive(true);
+            stage2Enemy.SetActive(false);
+            stage3Enemy.SetActive(false);
+
+            if (clearState == 1)
+                stamp.SetActive(true);
+            else
+                stamp.SetActive(false);
+
+            locker.SetActive(false);
+
+            subText.text = "복도와 인문대를 지나";
+            numText.text = "Stage 1";
+            infoText.text = "대학교를 탈출하기 위해선 3개의 STAGE를 통과해야 한다. 우리에겐 시간이 촉박하다.\n" +
+                "STAGE 1 - 복도와 인문대를 지나기 위해 드디어, 발걸음을 옯기게 되는데...";
+
+            SetStar(1);
+        }
+    }
+
+    public void OnStage2Button()
+    {
+        if (!isTransition && selectedStage != "Stage2")
+        {
+            selectedStage = "Stage2";
+            StartCoroutine("Fadeout");
+        }
+        else
+        {
+            stage1Button.GetComponent<Image>().sprite = disableMark;
+            stage2Button.GetComponent<Image>().sprite = enableMark;
+            stage3Button.GetComponent<Image>().sprite = disableMark;
+
+            time.text = "13:00";
+
+            stage1Enemy.SetActive(false);
+            stage2Enemy.SetActive(true);
+            stage3Enemy.SetActive(false);
+
+            if (clearState == 2)
+                stamp.SetActive(true);
+            else
+                stamp.SetActive(false);
+
+            if (clearState > 0)
+                locker.SetActive(false);
+            else
+                locker.SetActive(true);
+
+            subText.text = "자연대를 거쳐";
+            numText.text = "Stage 2";
+            infoText.text = "아직 긴장의 끈을 놓을 수 없다.\n" +
+                "STAGE 2는 STAGE 1의 비해 난이도가 높다고 알려져 있다. 자연대가 이렇게 무서웠나... 분위기가 매우 스산하고 어둡다.";
+
+            SetStar(3);
+        }
+    }
+
+    public void OnStage3Button()
+    {
+        if (!isTransition && selectedStage != "Stage3")
+        {
+            selectedStage = "Stage3";
+            StartCoroutine("Fadeout");
+        }
+        else
+        {
+            stage1Button.GetComponent<Image>().sprite = disableMark;
+            stage2Button.GetComponent<Image>().sprite = disableMark;
+            stage3Button.GetComponent<Image>().sprite = enableMark;
+
+            time.text = "16:00";
+
+            stage1Enemy.SetActive(false);
+            stage2Enemy.SetActive(false);
+            stage3Enemy.SetActive(true);
+
+            if (clearState == 3)
+                stamp.SetActive(true);
+            else
+                stamp.SetActive(false);
+
+            if (clearState > 1)
+                locker.SetActive(false);
+            else
+                locker.SetActive(true);
+
+            subText.text = "미대와 복도, 그리고 옥상";
+            numText.text = "Stage 3";
+            infoText.text = "마지막 STAGE까지 도달했다.\n여기서 멈출 수 없다.\n" +
+                "미대와 복도를 거쳐 옥상 어딘가에 최종 보스가 우리를 기다리고 있다고 하는데...";
+
+            SetStar(5);
+        }
+    }
+
+    public void StartGame()
+    {
+        if (locker.activeSelf == false)
+            SceneManager.LoadScene(selectedStage);
+        else
+            StartCoroutine("Lock");
+    }
+
+    IEnumerator Fadeout()
+    {
+        transition.transform.localScale = new Vector3(1, 2, 1);
+        while (transition.value < 1)
+        {
+            transition.value += 0.1f;
+            yield return new WaitForSeconds(0.01f);
+        }
+        yield return new WaitForSeconds(0.1f);
+
+        isTransition = true;
+        if (selectedStage == "Stage1")
+            OnStage1Button();
+        else if (selectedStage == "Stage2")
+            OnStage2Button();
+        else
+            OnStage3Button();
+
+        transition.transform.localScale = new Vector3(-1, 2, 1);
+        while (transition.value > 0)
+        {
+            transition.value -= 0.1f;
+            yield return new WaitForSeconds(0.01f);
+        }
+        isTransition = false;
+    }
+
+    IEnumerator Lock()
+    {
+        for(int i = 0; i < 5; i++)
+        {
+            locker.transform.localPosition = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), 0);
+            yield return new WaitForSeconds(0.05f);
+        }
+    }
+}
