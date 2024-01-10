43f411a549d0f4974e4fee7a16ecbae95caf2161
mins-oo <msjhkim04@naver.com>
2024-01-10 18:58:12 +0900

    fix: 전등 패턴 추가, 코인 관련 오류 수정
    LightTrap.cs
    3가지 패턴의 코루틴을 랜덤 호출하는 방식으로 변경
    
    Player.cs
    코인의 획득량(coin)이 dressState의 수를 초과하는 것을 방지
    코인을 먹은 상태에서 데미지를 입었다면 체력 대신 코인이 감소

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -211,6 +211,9 @@ public class Player : MonoBehaviour
 
     void DressState()
     {
+        if(coin > 2)
+            coin = 2;
+
         //character dress state
         for (int i = 0; i < dressState.Length; i++)
         {
@@ -247,7 +250,11 @@ public class Player : MonoBehaviour
 
     public void GetDamage(float damage)
     {
-        curHealth -= damage;
+        if (coin > 0)
+            coin--;
+        else
+            curHealth -= damage;
+
         otherAudio.clip = damageSound;
         otherAudio.Play();
     }
