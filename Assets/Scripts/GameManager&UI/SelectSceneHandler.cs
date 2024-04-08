95c1ed2c424b64b28ea6c05c41356183d59c3f6d
yenn023 <kimyeeun023@gmail.com>
2024-04-09 00:05:02 +0900

    feat: 아이템 설명 페이지 추가

--- Assets/Scripts/GameManager&UI/SelectSceneHandler.cs
+++ Assets/Scripts/GameManager&UI/SelectSceneHandler.cs
@@ -20,14 +20,20 @@ public class SelectSceneHandler : MonoBehaviour
     public Sprite stage1Image;
     public Sprite stage2Image;
     public Sprite stage3Image;
+    public Sprite disableMarkItem;
+    public Sprite enableMarkItem;
 
     [Header("Bookmark")]
     public GameObject stage1Button;
     public GameObject stage2Button;
     public GameObject stage3Button;
+    public GameObject itemButton;
+    public GameObject startButton;
+    public GameObject startImage;
 
     [Header("Timer")]
     public Text time;
+    public GameObject timer;
 
     [Header("Enemy")]
     public GameObject stage1Enemy;
@@ -42,8 +48,10 @@ public class SelectSceneHandler : MonoBehaviour
     public Text subText;
     public Text numText;
     public Text infoText;
+    public GameObject allTexts;
     public List<GameObject> stars;
     public GameObject stageImage;
+    public GameObject allStars;
 
     [Header("Transition")]
     public Slider transition;
@@ -54,7 +62,18 @@ public class SelectSceneHandler : MonoBehaviour
     [Header("Audio")]
     public AudioSource audioSource;
 
+    [Header("Item Page")]
+    public GameObject allItemPage;
+    public GameObject itemPage1;
+    public GameObject page1right;
+    public GameObject itemPage2;
+    public GameObject page2left;
+    public GameObject page2right;
+    public GameObject itemPage3;
+    public GameObject page3left;
+
     string selectedStage;
+    string itemPageNum;
 
     private void Start()
     {
@@ -88,7 +107,9 @@ public class SelectSceneHandler : MonoBehaviour
             stage1Button.GetComponent<Image>().sprite = enableMark;
             stage2Button.GetComponent<Image>().sprite = disableMark;
             stage3Button.GetComponent<Image>().sprite = disableMark;
+            itemButton.GetComponent<Image>().sprite = disableMarkItem;
 
+            timer.SetActive(true);
             time.text = "7:00";
 
             stage1Enemy.SetActive(true);
@@ -102,14 +123,22 @@ public class SelectSceneHandler : MonoBehaviour
 
             locker.SetActive(false);
 
+            allTexts.SetActive(true);
             subText.text = "복도와 인문대를 지나";
             numText.text = "Stage 1";
             infoText.text = "대학교를 탈출하기 위해선 3개의 STAGE를 통과해야 한다. 우리에겐 시간이 촉박하다.\n" +
                 "STAGE 1 - 복도와 인문대를 지나기 위해 드디어, 발걸음을 옯기게 되는데...";
 
+            allStars.SetActive(true);
             SetStar(1);
 
+            stageImage.SetActive(true);
             stageImage.GetComponent<Image>().sprite = stage1Image;
+
+            startButton.SetActive(true);
+            startImage.SetActive(true);
+
+            allItemPage.SetActive(false);
         }
     }
 
@@ -124,7 +153,9 @@ public class SelectSceneHandler : MonoBehaviour
             stage1Button.GetComponent<Image>().sprite = disableMark;
             stage2Button.GetComponent<Image>().sprite = enableMark;
             stage3Button.GetComponent<Image>().sprite = disableMark;
+            itemButton.GetComponent<Image>().sprite = disableMarkItem;
 
+            timer.SetActive(true);
             time.text = "10:00";
 
             stage1Enemy.SetActive(false);
@@ -141,14 +172,22 @@ public class SelectSceneHandler : MonoBehaviour
             else
                 locker.SetActive(true);
 
+            allTexts.SetActive(true);
             subText.text = "자연대를 거쳐";
             numText.text = "Stage 2";
             infoText.text = "아직 긴장의 끈을 놓을 수 없다.\n" +
                 "STAGE 2는 STAGE 1에 비해 난이도가 높다고 알려져 있다. 자연대가 이렇게 무서웠나... 분위기가 매우 스산하고 어둡다.";
 
+            allStars.SetActive(true);
             SetStar(3);
 
+            stageImage.SetActive(true);
             stageImage.GetComponent<Image>().sprite = stage2Image;
+
+            startButton.SetActive(true);
+            startImage.SetActive(true);
+
+            allItemPage.SetActive(false);
         }
     }
 
@@ -163,7 +202,9 @@ public class SelectSceneHandler : MonoBehaviour
             stage1Button.GetComponent<Image>().sprite = disableMark;
             stage2Button.GetComponent<Image>().sprite = disableMark;
             stage3Button.GetComponent<Image>().sprite = enableMark;
+            itemButton.GetComponent<Image>().sprite = disableMarkItem;
 
+            timer.SetActive(true);
             time.text = "13:00";
 
             stage1Enemy.SetActive(false);
@@ -180,14 +221,22 @@ public class SelectSceneHandler : MonoBehaviour
             else
                 locker.SetActive(true);
 
+            allTexts.SetActive(true);
             subText.text = "미대와 복도, 그리고 옥상";
             numText.text = "Stage 3";
             infoText.text = "마지막 STAGE까지 도달했다.\n여기서 멈출 수 없다.\n" +
                 "미대와 복도를 거쳐 옥상 어딘가에 최종 보스가 우리를 기다리고 있다고 하는데...";
 
+            allStars.SetActive(true);
             SetStar(5);
 
+            stageImage.SetActive(true);
             stageImage.GetComponent<Image>().sprite = stage3Image;
+
+            startButton.SetActive(true);
+            startImage.SetActive(true);
+
+            allItemPage.SetActive(false);
         }
     }
 
@@ -232,5 +281,82 @@ public class SelectSceneHandler : MonoBehaviour
             yield return new WaitForSeconds(0.05f);
         }
         locker.transform.localPosition = Vector3.zero;
+    }
+
+    public void OnItemButton()
+    {
+        if (selectedStage != "Item")
+        {
+            selectedStage = "Item";
+
+            Debug.Log("item");
+
+            PlaySound();
+
+            stage1Button.GetComponent<Image>().sprite = disableMark;
+            stage2Button.GetComponent<Image>().sprite = disableMark;
+            stage3Button.GetComponent<Image>().sprite = disableMark;
+            itemButton.GetComponent<Image>().sprite = enableMarkItem;
+
+            timer.SetActive(false);
+
+            stage1Enemy.SetActive(false);
+            stage2Enemy.SetActive(false);
+            stage3Enemy.SetActive(false);
+
+            stageImage.SetActive(false);
+
+            stamp.SetActive(false);
+
+            allStars.SetActive(false);
+
+            allTexts.SetActive(false);
+
+            startButton.SetActive(false);
+            startImage.SetActive(false);
+
+            allItemPage.SetActive(true);
+
+            FirstItemPage();
+        }
+    }
+
+    public void FirstItemPage()
+    {
+        itemPage1.SetActive(true);
+        itemPage2.SetActive(false);
+        itemPage3.SetActive(false);
+
+        if (itemPageNum != "Page1")
+        {
+            itemPageNum = "Page1";
+            Debug.Log("1");
+        }
+    }
+
+    public void SecondItemPage()
+    {
+        itemPage1.SetActive(false);
+        itemPage2.SetActive(true);
+        itemPage3.SetActive(false);
+
+        if (itemPageNum != "Page2")
+        {
+            itemPageNum = "Page2";
+            Debug.Log("2");
+        }
+    }
+
+    public void ThridItemPage()
+    {
+        itemPage1.SetActive(false);
+        itemPage2.SetActive(false);
+        itemPage3.SetActive(true);
+
+        if (itemPageNum != "Page3")
+        {
+            itemPageNum = "Page3";
+            Debug.Log("3");
+        }
     }
 }
