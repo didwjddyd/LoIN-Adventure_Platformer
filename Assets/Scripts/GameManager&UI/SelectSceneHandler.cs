f1971b5f7a5a1194d99f28135479f63eee7b6ab5
didwjddyd <48481171+didwjddyd@users.noreply.github.com>
2024-06-09 23:46:24 +0900

    Revert "fix: 스테이지 선택씬 아이템페이지 클릭 효과음 변경"

--- Assets/Scripts/GameManager&UI/SelectSceneHandler.cs
+++ Assets/Scripts/GameManager&UI/SelectSceneHandler.cs
@@ -370,7 +370,7 @@ public class SelectSceneHandler : MonoBehaviour
         itemPage1.SetActive(true);
         itemPage2.SetActive(false);
         itemPage3.SetActive(false);
-        PlaySound();
+        itemPageAudio.Play();
 
         if (itemPageNum != "Page1")
         {
@@ -384,7 +384,7 @@ public class SelectSceneHandler : MonoBehaviour
         itemPage1.SetActive(false);
         itemPage2.SetActive(true);
         itemPage3.SetActive(false);
-        PlaySound();
+        itemPageAudio.Play();
 
         if (itemPageNum != "Page2")
         {
@@ -398,7 +398,7 @@ public class SelectSceneHandler : MonoBehaviour
         itemPage1.SetActive(false);
         itemPage2.SetActive(false);
         itemPage3.SetActive(true);
-        PlaySound();
+        itemPageAudio.Play();
 
         if (itemPageNum != "Page3")
         {
